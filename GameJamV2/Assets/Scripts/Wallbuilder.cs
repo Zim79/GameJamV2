3bf4d598cbf886d7e0cf06b980d6bc7128599416
gustav2302 <gjm2302@gmail.com>
2018-08-07 00:27:03 +0200

    nada

---
+++ GameJamV2/Assets/Scripts/Wallbuilder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wallbuilder : MonoBehaviour {
+
+	private Mesh mesh;
+	private GameObject wall;
+	private Vector3[] vert;
+	private int[] tri;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
