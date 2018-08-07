92f31e8d42cc0181c71e7e8d1937aadbf45dde78
Zim79 <simon.d.laursen.o.o@gmail.com>
2018-08-07 13:05:27 +0200

    Export UI

---
+++ GameJamV2/Assets/Building and UI/BuildingPlacement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingPlacement : MonoBehaviour {
+
+    public GameObject[] Buildings = new GameObject[4];
+
+    Vector3 clickPosInWorldCoordinates;
+    Coroutine waitForClick;
+    GameObject buildingToPlace;
+    bool buildMode;
+
+	void Start () 
+    {
+		
+	}
+
+    public void InitiateBuildMode(Building buildingType) 
+    {
+        buildMode = true;
+        waitForClick = StartCoroutine("SelectBuildingPlacement");
+    }
+
+    IEnumerator SelectBuildingPlacement() 
+    {
+        while (buildMode)
+        {
+            if(buildingToPlace == null)
+            {
+                buildingToPlace = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                buildingToPlace.layer=2;
+            }
+
+            RaycastHit hit;
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000f, 1);
+            if (hit.collider != null)
+            {
+                buildingToPlace.transform.position = hit.point;
+            }
+
+            if (Input.touchCount != 0 || Input.GetAxis("RightClick") == 1)
+            {
+                buildMode = false;
+                buildingToPlace = null;
+            }
+
+
+            yield return null;
+        }
+        if (!buildMode)
+            StopCoroutine(waitForClick);
+    }
+}
+
+public enum Building {
+    None,
+    TownHall,
+    House,
+    Tower
+}
---
+++ GameJamV2/Assets/Building and UI/UIHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour {
+
+    public GameObject BuildPanel;
+    public BuildingPlacement BuildingPlacementScript;
+
+    Building selectedBuildingType;
+
+	void Start () {
+		
+	}
+	
+    public void OpenBuildUI() {
+        if (!BuildPanel.activeInHierarchy)
+            BuildPanel.SetActive(true);
+    }
+    public void CloseBuildUI() {
+        if (BuildPanel.activeInHierarchy)
+            BuildPanel.SetActive(false);
+    }
+    public void BuildTownHall() {
+        selectedBuildingType = Building.TownHall;
+        Build();
+    }
+    public void BuildTower() {
+        selectedBuildingType = Building.Tower;
+        Build();
+    }
+    public void BuildHouse() {
+        selectedBuildingType = Building.House;
+        Build();
+    }
+
+    void Build() {
+        BuildingPlacementScript.InitiateBuildMode(selectedBuildingType);
+        selectedBuildingType = Building.None;
+    }
+}
