3e20ce42a811e2c3db9c47434f264c1b97076d6c
gustav2302 <gjm2302@gmail.com>
2018-08-07 17:07:48 +0200

    updated

--- GameJamV2/Assets/Scripts/Wallbuilder.cs
+++ GameJamV2/Assets/Scripts/Wallbuilder.cs
@@ -16,27 +16,62 @@ public class Wallbuilder : MonoBehaviour {
 	List<CombineInstance>[] combine;
 	private float thick;
 	public Material mat;
+	public bool build = false;
+	public List<Vector3> wPos;
+	public List<Quaternion> wRot;
+	public Vector3[] av;
+	private int num;
+	private Vector3[] vert;
 
 	void Start ()
 	{
 		mesh = new List<Mesh>();
-		Build();
 	}
-	
-	void Build ()
+
+	private void Update()
 	{
+		if (build)
+		{
+			build = false;
+			Build();
+			Width();
+		}
+	}
+
+	public void Build ()
+	{
+		num = -1;
+		for (int i = 0; i < wPos.Count; i++)
+		{
+			if (Vector3.Distance(pos1, wPos[i]) <= 2)
+			{
+				pos1 = wPos[i];
+				num = i;
+			}
+			else if (Vector3.Distance(pos2, wPos[i]) <= 2)
+			{
+				pos2 = wPos[i];
+			}
+		}
 		spawned = Instantiate(wall, pos1, Quaternion.Euler(-90, 0, 0));
 		thick = spawned.GetComponent<Renderer>().bounds.size.z / 2;
-		dist = (Vector2.Distance(new Vector2(pos1.x, pos1.z), new Vector2(pos2.x, pos2.z)) / (spawned.GetComponent<Renderer>().bounds.size.z));
 		spawned.transform.LookAt(pos2);
 		spawned.transform.Rotate(-90,0,0);
 		Quaternion qua = spawned.transform.rotation;
+		if (num != -1)
+		{
+			vert = new Vector3[5];
+			vert[0] = pos1 - wRot[num] * new Vector3(1.624999f, 0, 0);
+		}
+		dist = (Vector2.Distance(new Vector2(pos1.x, pos1.z), new Vector2(pos2.x, pos2.z)) / (spawned.GetComponent<Renderer>().bounds.size.z));
 		Destroy(spawned);
 		twoDist = pos2 - pos1;
 		combine = new List<CombineInstance>[Mathf.CeilToInt(dist / 20)];
+		av = new Vector3[Mathf.CeilToInt(dist / 20)];
 		for (int i = 0; i < Mathf.CeilToInt(dist / 20); i++)
 		{
 			combine[i] = new List<CombineInstance>();
+			av[i] = Vector3.zero;
 		}
 		for (int i = 0; i < dist; i++)
 		{
@@ -51,18 +86,64 @@ public class Wallbuilder : MonoBehaviour {
 			spawned.transform.Translate(0, -thick, 0, Space.Self);
 			CombineInstance comb = new CombineInstance();
 			comb.mesh = spawned.GetComponent<MeshFilter>().mesh;
-			comb.transform = spawned.transform.localToWorldMatrix;
+			Matrix4x4 matrix = spawned.transform.localToWorldMatrix;
+			spawned.transform.RotateAround(Vector3.zero, Vector3.up, -qua.eulerAngles.y);
+			matrix.SetTRS(spawned.transform.position, spawned.transform.rotation, spawned.transform.localScale);
+			comb.transform = matrix;
 			combine[Mathf.CeilToInt(i / 20)].Add(comb);
+
 			Destroy(spawned);
 		}
 		for (int i = 0; i < Mathf.CeilToInt(dist / 20); i++)
 		{
-			spawned = Instantiate(wall, Vector3.zero, Quaternion.identity);
+			spawned = Instantiate(wall, Vector3.zero, Quaternion.Euler(0, qua.eulerAngles.y, 0));
 			spawned.transform.localScale = new Vector3(1, 1, 1);
-			spawned.GetComponent<MeshFilter>().mesh = new Mesh();
-			spawned.GetComponent<MeshFilter>().mesh.CombineMeshes(combine[i].ToArray());
+			Mesh mesh = new Mesh();
+			wPos.Add(Quaternion.Euler(0, qua.eulerAngles.y, 0) * (combine[i][0].transform.MultiplyPoint3x4(Vector3.zero) - new Vector3(0, 0, thick)));
+			wRot.Add(Quaternion.Euler(0, qua.eulerAngles.y, 0));
+			wPos.Add(Quaternion.Euler(0, qua.eulerAngles.y, 0) * (combine[i][combine[i].Count - 1].transform.MultiplyPoint3x4(Vector3.zero) + new Vector3(0, 0, thick)));
+			wRot.Add(Quaternion.Euler(0, -qua.eulerAngles.y, 0));
+			mesh.CombineMeshes(combine[i].ToArray(), true, true);
+			//mesh.ClearBlendShapes();
+			spawned.GetComponent<MeshFilter>().mesh = mesh;
 			spawned.GetComponent<Renderer>().material = mat;
 			spawned.AddComponent<MeshCollider>();
+		}
+	}
+
+	public void Width()
+	{
+		Mesh m = spawned.GetComponent<MeshFilter>().mesh;
+		Vector3[] v = m.vertices;
+		float n = 0;
+		float s = 1000;
+		for (int i = 0; i < v.Length; i++)
+		{
+			if (v[i].x < s && v[i].y < 0)
+			{
+				s = v[i].x;
+			}
+			else if (v[i].x > n && v[i].y < 0)
+			{
+				n = v[i].x;
+			}
+		}
+		print(n);
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.cyan;
+		foreach (Vector3 wPo in wPos)
+		{
+			Gizmos.DrawSphere(wPo, 0.5f);
+		}
+		if (vert != null)
+		{
+			foreach (Vector3 ver in vert)
+			{
+				Gizmos.DrawSphere(ver, 0.2f);
+			}
 		}
 	}
 }
